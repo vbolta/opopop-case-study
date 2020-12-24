af7167241faf91badbbc48a068ee27caa53e60fd
Victor Bolta <victorbolta@macbook-pro-de-victor.home>
2020-12-24 17:50:56 +0100

    Adding last elements

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
